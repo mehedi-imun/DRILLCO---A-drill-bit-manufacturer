07a1a013c8afb8633900d3bd8fee47432a660a38
mahedi <mahedianik4@gmail.com>
2022-05-26 04:20:05 +0600

    add my review page

--- src/components/Pages/Dashboard/MyReview.js
+++ src/components/Pages/Dashboard/MyReview.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 
 const MyReview = () => {
+    const [error, setError] = useState('')
+    const handleReview = (event) => {
+        event.preventDefault()
+        const rating = parseInt(event.target.ratingNum.value)
+        const description = event.target.description.value
+        const review = 
+            {
+                rating: rating,
+                description: description
+            }
+        
+        if (rating <= 5) {
+            fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({review})
+
+        })
+            .then(res => res.json())
+            .then(data=>{
+                if(data.acknowledged){
+                    toast.success('successfully review added')
+                    event.target.description.value=''
+                    event.target.ratingNum.value=''
+                }
+            })
+
+        } else {
+            setError('maximum rating-5 star')
+        }
+
+    }
     return (
-        <div>
-            
+        <div class="card w-96 bg-base-100 shadow-xl ml-16 mt-10">
+            <div class="card-body ">
+                <ToastContainer></ToastContainer>
+                <div class="rating">
+                    <form onSubmit={handleReview}>
+                        {error}
+                        <input name='ratingNum' type="number" placeholder="Enter your rating" class="input input-bordered input-sm w-full max-w-xs bg-base-100" />
+                        <textarea name='description' type="text" placeholder="Enter your rating" class="textarea border  w-full max-w-xs  textarea-accent mt-5" />
+                        <input type="submit" className='btn w-full  btn-sm btn-secondary' value='add' />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Home/ReviewCard.js
+++ src/components/Pages/Home/ReviewCard.js
@@ -3,20 +3,20 @@ import Rating from 'react-rating';
 import { BsFillStarFill } from 'react-icons/bs';
 
 const ReviewCard = ({ review }) => {
-    const { name, description, rating } = review;
+    const { description, rating } = review;
     return (
         <div className='border w-80 p-5 lg:p-0 rounded-md '>
             <div className=''>
-                <h4 className='p-5'>Name: {name}</h4>
-            <span className='p-5'> Ratings: <Rating
+                
+            <div className='p-5'> Ratings: <Rating
                 initialRating={rating}
                 emptySymbol={<BsFillStarFill icon={BsFillStarFill} />}
                 fullSymbol={<BsFillStarFill style={{ color: 'goldenrod' }} icon={BsFillStarFill} />}
                 readonly
-            ></Rating></span>
+            ></Rating></div>
             </div>
           
-                <p className='mt-5 p-5'>{description}</p>
+                <p className=' p-5'>{description}</p>
             
 
         </div>
